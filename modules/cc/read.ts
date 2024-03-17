e4a42693395e3fe2d09a7a80614b548ea54718a9
Daniel Wei <weidaniel15@gmail.com>
2024-03-17 02:19:49 -0400

    Remove Outdated Tests

--- modules/cc/read.ts
+++ modules/cc/read.ts
@@ -3,9 +3,9 @@ import { bunReadFile } from "../stdlib/bun-effect";
 import parse from "../parser/parse";
 
 /**
- * Constructs a single fiber that reads the logs for a student to calculate their grade and returns it.
+ * Constructs a single fiber that reads the logs for a student to calculate their grade and returns it, along with their netID and a comment.
  * @param netID The student's netID
- * @returns An Effect that resolves to a tuple of the netID and grade.
+ * @returns An Effect that resolves to a GradebookEntry.
  */
 const readLogs = (netID: string, filePath: string) =>
     Effect.gen(function* ($) {
